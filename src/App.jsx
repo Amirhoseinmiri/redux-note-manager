6c6893e6767b39da3685df4444a8a394df184c16
Amir <A.Miri>
2024-06-05 14:38:52 +0330

    initial changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { Layout } from "components/Layout/Layout";
+import { Note } from "pages/Note/Note";
+import { NoteBrowse } from "pages/NoteBrowse/NoteBrowse";
+import { NoteCreate } from "pages/NoteCreate/NoteCreate";
+import { PageNotFound } from "pages/PageNotFound/PageNotFound";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export function App() {
-  return <div>React Redux Starter :-)</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<NoteBrowse />} />
+            <Route path="/note/:id" element={<Note />} />
+            <Route path="/note/new" element={<NoteCreate />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
-// import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { noteReducer } from "./notes-slice";
 
-// export const store = configureStore({
-//   reducer: {},
-// });
+export const store = configureStore({
+  reducer: {
+    noteSlice: noteReducer,
+  },
+});
